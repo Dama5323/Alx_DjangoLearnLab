8e5ed326513fecd78efcf9741ab400064942468d
Dama5323 <your-deenyashke@gmail.com>
2025-07-26 04:47:26 +0300

    Django Views and URL Configuration

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from django.contrib.auth.views import LoginView, LogoutView
+from .views import list_books
 
 urlpatterns = [
     path('register/', views.register, name='register'),  # ✅ match: views.register
