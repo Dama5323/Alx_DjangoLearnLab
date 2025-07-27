fc9b132ddd1d46641e6896fe2a8bb274f8af94a5
Dama5323 <your-deenyashke@gmail.com>
2025-07-27 18:01:32 +0300

    Implementing Security Best Practices in Django

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -7,6 +7,8 @@ from django.contrib.auth.decorators import permission_required
 from .models import Book
 from django.core.exceptions import PermissionDenied
 from django.contrib.auth.decorators import login_required
+from .forms import ExampleForm
+
 
 @permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
@@ -58,3 +60,13 @@ def raise_exception(request):
 def books(request):
     return render(request, 'bookshelf/book_list.html')  # reuse this view if needed
 
+def example_form_view(request):
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')  # or wherever you want to go
+    else:
+        form = ExampleForm()
+
+    return render(request, 'bookshelf/form_example.html', {'form': form})
