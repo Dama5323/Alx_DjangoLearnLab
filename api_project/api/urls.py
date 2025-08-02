373956be25005fbb53bc616e4b323b48766fea33
Dama5323 <your-deenyashke@gmail.com>
2025-08-02 17:38:35 +0300

    Develop a simple API endpoint using Django REST Framework that allows clients to retrieve information about books stored in your database

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import BookListCreate
+from .views import BookList
 
 urlpatterns = [
     path('books/', BookListCreate.as_view(), name='book-list-create'),
+    path('books/', BookList.as_view(), name='book-list'),
 ]
