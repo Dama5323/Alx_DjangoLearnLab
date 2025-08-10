3fb9479735eb640f74aaa2b862feb64c92e3dc98
Dama5323 <your-deenyashke@gmail.com>
2025-08-10 18:18:55 +0300

    Writing Unit Tests for Django REST Framework APIs

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -5,6 +5,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from .models import Author, Book
 from .serializers import AuthorSerializer, BookSerializer
 from .permissions import IsCreatorOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 
 class CustomAPIResponseMixin:
