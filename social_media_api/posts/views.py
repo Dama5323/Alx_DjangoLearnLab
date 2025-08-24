93e603e86d3a81ab5f6270eb395b61d4d9bb3dc7
Dama5323 <your-deenyashke@gmail.com>
2025-08-24 19:37:53 +0300

    Implementing Notifications and Likes Functionality

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -35,9 +35,9 @@ class PostViewSet(viewsets.ModelViewSet):
     
     @action(detail=True, methods=['post'])
     def like(self, request, pk=None):
-        post = get_object_or_404(Post, pk=pk) 
+        post = generics.get_object_or_404(Post, pk=pk)  # Changed to generics.get_object_or_404
         user = request.user
-        like, created = Like.objects.get_or_create(user=user, post=post)
+        like, created = Like.objects.get_or_create(user=request.user, post=post)  # Changed to request.user
         
         if not created:
             return Response({"error": "You have already liked this post."}, status=status.HTTP_400_BAD_REQUEST)
@@ -58,11 +58,11 @@ class PostViewSet(viewsets.ModelViewSet):
     
     @action(detail=True, methods=['post'])
     def unlike(self, request, pk=None):
-        post = get_object_or_404(Post, pk=pk)
+        post = generics.get_object_or_404(Post, pk=pk)  # Changed to generics.get_object_or_404
         user = request.user
         
         try:
-            like = Like.objects.get(user=user, post=post)
+            like = Like.objects.get(user=request.user, post=post)  # Changed to request.user
             like.delete()
             return Response({"message": "Post unliked successfully."}, status=status.HTTP_200_OK)
         except Like.DoesNotExist:
@@ -152,11 +152,9 @@ class LikePostView(generics.CreateAPIView):
     permission_classes = [IsAuthenticated]
     
     def perform_create(self, serializer):
-        post = get_object_or_404(Post, pk=self.kwargs['pk'])
-        like, created = Like.objects.get_or_create(
-            user=self.request.user, 
-            post=post
-        )
+        post = generics.get_object_or_404(Post, pk=self.kwargs['pk'])  # Changed to generics.get_object_or_404
+        like, created = Like.objects.get_or_create(user=self.request.user, post=post)  # Changed to self.request.user
+        
         if not created:
             raise ValidationError("You have already liked this post.")
         
@@ -176,9 +174,9 @@ class UnlikePostView(generics.DestroyAPIView):
     permission_classes = [IsAuthenticated]
     
     def delete(self, request, *args, **kwargs):
-        post = get_object_or_404(Post, pk=self.kwargs['pk'])
+        post = generics.get_object_or_404(Post, pk=self.kwargs['pk'])  # Changed to generics.get_object_or_404
         try:
-            like = Like.objects.get(user=request.user, post=post)
+            like = Like.objects.get(user=request.user, post=post)  # Changed to request.user
             like.delete()
             return Response({"message": "Post unliked successfully."}, status=status.HTTP_200_OK)
         except Like.DoesNotExist:
